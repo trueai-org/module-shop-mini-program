c8558fcbeb78e67f18aa26d7ff84f7ecebfcd04a
zhangkp <791050055@qq.com>
2019-04-13 11:49:03 +0800

    1

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 const ApiRootUrl = 'http://127.0.0.1:8360/api/';
-// const ApiHost = 'https://localhost:44372/api/';
-const ApiHost = 'https://circle.ac.cn/api/';
+const ApiHost = 'https://localhost:44372/api/';
+// const ApiHost = 'https://circle.ac.cn/api/';
 
 module.exports = {
   // 首页
